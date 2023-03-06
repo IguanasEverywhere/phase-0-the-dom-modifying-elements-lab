4fa702fdcfd3ffb38a5db99488dae6988a44c889
Scott Schwab <scottschwab86@gmail.com>
2023-03-06 16:16:09 -0600

    Finish modifying elements thru JS

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement = document.querySelector("#main");
+mainElement.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = 'Scott is the champion';
